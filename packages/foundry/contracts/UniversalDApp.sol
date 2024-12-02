1b02a76fc370b89f0a71dd56caea1ce9210afd67
nzmpi <nizami.abdulov@gmail.com>
2024-12-02 17:53:56 +0400

    feat: add Bitcoin support

--- packages/foundry/contracts/UniversalDApp.sol
+++ packages/foundry/contracts/UniversalDApp.sol
@@ -15,16 +15,18 @@ contract UniversalDApp is IUniversalContract {
     using SwapHelperLib for SystemContract;
     using BytesHelperLib for bytes;
 
-    SystemContract public systemContract;
-    IGatewayZEVM public gateway;
-    uint256 constant BITCOIN = 18332;
+    SystemContract public immutable systemContract;
+    IGatewayZEVM public immutable gateway;
+    address constant BITCOIN = 0x65a45c57636f9BcCeD4fe193A602008578BcA90b;
+    uint256 constant BITCOIN_CHAIN_ID = 18332;
 
-    event Reverted(bytes recipient, address asset, uint256 amount);
-    event SwappedAndWithdrawn();
+    event Reverted(bytes indexed recipient, address asset, uint256 amount);
+    event SwappedAndWithdrawn(bytes indexed recipient, address asset, uint256 amount);
 
     error InvalidAddress();
     error InvalidChainToken();
     error NotGateway();
+    error NotSupportedChainID();
 
     modifier onlyGateway() {
         if (msg.sender != address(gateway)) revert NotGateway();
@@ -41,6 +43,7 @@ contract UniversalDApp is IUniversalContract {
         address targetChainToken;
         uint256 gasLimit;
         uint256 minAmount;
+        address originalSender;
         bytes targetChainCounterparty;
         bytes destinationPayload;
     }
@@ -49,18 +52,18 @@ contract UniversalDApp is IUniversalContract {
         external
         onlyGateway
     {
-        Params memory params;
-        if (context.chainID == BITCOIN) {
-            // TODO: fix other params
-            params.targetChainToken = message.bytesToAddress(0);
-            params.targetChainCounterparty = bytes.concat(bytes20(message.bytesToAddress(20)));
-        } else {
-            params = abi.decode(message, (Params));
+        if (context.chainID == BITCOIN_CHAIN_ID) {
+            revert NotSupportedChainID();
         }
 
-        if (params.targetChainToken == address(0)) revert InvalidChainToken();
-
-        swapAndWithdraw(zrc20, amount, params);
+        Params memory params = abi.decode(message, (Params));
+        if (params.targetChainToken == address(0)) {
+            revert InvalidChainToken();
+        } else if (params.targetChainToken == BITCOIN) {
+            swapAndWithdrawBTC(zrc20, amount, params);
+        } else {
+            swapAndWithdrawEVM(zrc20, amount, params);
+        }
     }
 
     function onRevert(RevertContext calldata revertContext) external onlyGateway {
@@ -79,12 +82,11 @@ contract UniversalDApp is IUniversalContract {
             IZRC20(targetToken).approve(address(gateway), outputAmount);
         }
 
-        // Decode the revert message
-        bytes memory recipient = abi.decode(revertContext.revertMessage, (bytes));
+        bytes memory recipient = revertContext.revertMessage;
 
         RevertOptions memory revertOptions = RevertOptions({
             revertAddress: address(this),
-            callOnRevert: true,
+            callOnRevert: false,
             abortAddress: address(0),
             revertMessage: "",
             onRevertGasLimit: 0
@@ -96,41 +98,12 @@ contract UniversalDApp is IUniversalContract {
         emit Reverted(recipient, targetToken, outputAmount);
     }
 
-    function swapAndWithdraw(address inputToken, uint256 amount, Params memory params) internal {
+    function swapAndWithdrawEVM(address inputToken, uint256 amount, Params memory params) internal {
         // Get the gas fee required for withdrawal and call
         (address gasZRC20, uint256 gasFee) = IZRC20(params.targetChainToken).withdrawGasFeeWithGasLimit(params.gasLimit);
-        uint256 swapAmount;
-        // Calculate the amount left after covering gas fees
-        if (gasZRC20 == inputToken) {
-            swapAmount = amount - gasFee;
-        } else {
-            swapAmount = amount - systemContract.swapTokensForExactTokens(inputToken, gasFee, gasZRC20, amount);
-        }
 
-        // Perform the token swap if the input and target tokens are different
-        uint256 outputAmount = swapAmount;
-        if (inputToken != params.targetChainToken) {
-            outputAmount = systemContract.swapExactTokensForTokens(
-                inputToken, swapAmount, params.targetChainToken, params.minAmount
-            );
-        }
-
-        // Approve the gateway to spend the tokens
-        if (gasZRC20 == params.targetChainToken) {
-            IZRC20(gasZRC20).approve(address(gateway), outputAmount + gasFee);
-        } else {
-            IZRC20(gasZRC20).approve(address(gateway), gasFee);
-            IZRC20(params.targetChainToken).approve(address(gateway), outputAmount);
-        }
-
-        // Prepare the revert options
-        RevertOptions memory revertOptions = RevertOptions({
-            revertAddress: address(this),
-            callOnRevert: true,
-            abortAddress: address(0),
-            revertMessage: "",
-            onRevertGasLimit: 0
-        });
+        (uint256 outputAmount, RevertOptions memory revertOptions) =
+            swapAndApprove(gasZRC20, gasFee, inputToken, amount, params);
 
         // Execute the withdrawal and call operation via the gateway
         CallOptions memory callOptions = CallOptions(params.gasLimit, true);
@@ -143,6 +116,54 @@ contract UniversalDApp is IUniversalContract {
             revertOptions
         );
 
-        emit SwappedAndWithdrawn();
+        emit SwappedAndWithdrawn(params.targetChainCounterparty, params.targetChainToken, outputAmount);
+    }
+
+    function swapAndWithdrawBTC(address inputToken, uint256 amount, Params memory params) internal {
+        // Get the gas fee required for withdrawal and call
+        (address gasZRC20, uint256 gasFee) = IZRC20(params.targetChainToken).withdrawGasFee();
+
+        (uint256 outputAmount, RevertOptions memory revertOptions) =
+            swapAndApprove(gasZRC20, gasFee, inputToken, amount, params);
+
+        // Execute the withdrawal via the gateway
+        gateway.withdraw(params.targetChainCounterparty, outputAmount, params.targetChainToken, revertOptions);
+
+        emit SwappedAndWithdrawn(params.targetChainCounterparty, params.targetChainToken, outputAmount);
+    }
+
+    function swapAndApprove(address gasZRC20, uint256 gasFee, address inputToken, uint256 amount, Params memory params)
+        internal
+        returns (uint256 outputAmount, RevertOptions memory revertOptions)
+    {
+        uint256 swapAmount;
+        // Calculate the amount left after covering gas fees
+        if (gasZRC20 == inputToken) {
+            swapAmount = amount - gasFee;
+        } else {
+            swapAmount = amount - systemContract.swapTokensForExactTokens(inputToken, gasFee, gasZRC20, amount);
+        }
+
+        // Perform the token swap if the input and target tokens are different
+        outputAmount = inputToken != params.targetChainToken
+            ? systemContract.swapExactTokensForTokens(inputToken, swapAmount, params.targetChainToken, params.minAmount)
+            : swapAmount;
+
+        // Approve the gateway to spend the tokens
+        if (gasZRC20 == params.targetChainToken) {
+            IZRC20(gasZRC20).approve(address(gateway), outputAmount + gasFee);
+        } else {
+            IZRC20(gasZRC20).approve(address(gateway), gasFee);
+            IZRC20(params.targetChainToken).approve(address(gateway), outputAmount);
+        }
+
+        // Prepare the revert options
+        revertOptions = RevertOptions({
+            revertAddress: address(this),
+            callOnRevert: true,
+            abortAddress: address(0),
+            revertMessage: bytes.concat(bytes20(params.originalSender)),
+            onRevertGasLimit: 0
+        });
     }
 }
