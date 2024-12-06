d786f8ad69bbd9a742cf384151787394ba08e060
Jacob Homanics <homanicsjake@gmail.com>
2024-12-05 18:23:37 -0600

    up

--- packages/nextjs/components/InputBox.tsx
+++ packages/nextjs/components/InputBox.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import CategorySelect from "./CategorySelect";
 import CategorySelectInputBox from "./CategorySelectInputBox";
 import UserActionBoxContainer from "./UserActionBoxContainer";
@@ -63,20 +64,25 @@ const InputBox = () => {
 
   const comps = filteredNetworkOptions.map((e: any, index: number) => {
     return (
-      <div key={"sjf" + index}>
+      <div key={"sjf" + index} className="flex flex-col gap-1">
         <p className="m-0 text-xl text-bold">{e.section}</p>
         {e.options.map((option: any, index: number) => {
           return (
-            <div className="flex bg-base-100 rounded p-1 m-1 justify-between" key={"sndn" + index}>
-              <p className="m-0">{option.label}</p>
-              <button
-                className="m-0"
-                onClick={() => {
-                  updateSpecificOption(e.section, option.value, !option.selected);
-                }}
-              >
-                {"(X)"}
-              </button>
+            <div
+              className={`min-h-0 h-8 py-1 px-2 leading-tight shadow-inner-xl flex items-center w-full text-xs bg-[#3C3731] justify-between`}
+              key={"sndn" + index}
+            >
+              <div className="flex gap-1">
+                <button
+                  className="m-0"
+                  onClick={() => {
+                    updateSpecificOption(e.section, option.value, !option.selected);
+                  }}
+                >
+                  <Image src={"/Vector.png"} alt="" width={"12"} height={"12"} className="h-4" />
+                </button>
+                <p className="m-0">{option.label}</p>
+              </div>
             </div>
           );
         })}
@@ -131,7 +137,8 @@ const InputBox = () => {
           onChange={updateSpecificOption}
         />
       </div>
-      <div className="overflow-scroll h-40 p-1">
+      <div className="p-[0.4px] bg-[#FFFFFF] rounded my-3"></div>
+      <div className="overflow-scroll h-40">
         {comps}
         {/* <AllTokensPrices /> */}
         {/* <AllTokensBalances address="0xc0f0E1512D6A0A77ff7b9C172405D1B0d73565Bf" /> */}
